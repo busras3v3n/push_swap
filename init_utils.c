46bd05a632edd187a1460a3810307548aa765dc8
Büşra Seven <152426280+busras3v3n@users.noreply.github.com>
2025-02-12 15:09:12 +0300

    added init utils file

---
+++ init_utils.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 15:07:54 by busseven          #+#    #+#             */
+/*   Updated: 2025/02/12 15:08:57 by busseven         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	check_number_count(char **argv)
+{
+	int count;
+	int	i;
+	char **temp;
+
+	count = 0;
+	i = 1;
+	while(argv[i])
+	{
+		temp = ft_split(argv[i], ' ');
+		count += get_2d_length(temp);
+		free_2d_arr(temp);
+		i++;
+	}
+	return(count);
+}
+void	init_data_numbers(char **argv, t_data *data)
+{
+	char	**temp;
+	int		n;
+	int		i;
+	int		k;
+
+	i = 1;
+	n = 0;
+	data->numbers = ft_calloc(check_number_count(argv) + 1, sizeof(char *));
+	while(argv[i])
+	{
+		k = 0;
+		temp = ft_split(argv[i], ' ');
+		while(temp[k])
+		{
+			data->numbers[n] = temp[k];
+			n++;
+			k++;
+		}
+		free(temp);
+		i++;
+	}	
+}
+
+int		set_index(int content, t_list *a)
+{
+	int i;
+
+	i = 0;
+	while(a)
+	{
+		if(content > a->content)
+			i++;
+		else if(content < a->content)
+			(a->index)++;
+		a = a->next;
+	}
+	return(i);
+}
+
+void	put_nodes_in_a(t_list **a, char **numbers)
+{
+	t_list *new;
+	int	i;
+
+	i = 0;
+	while(numbers[i])
+	{
+		new = ft_lstnew(ft_atoi(numbers[i]));
+		new->index = set_index(new->content, *a);
+		new->target = -1;
+		new->cost = -1;
+		new->position = -1;
+		new->direction = -1;
+		new->is_first = -1;
+		ft_lstadd_back(a, new);
+		i++;
+	}
+}
+void	check_arguments(char **numbers, t_data *data)
+{
+	check_for_invalid_char(numbers, data);
+	check_for_non_int(numbers, data);
+	check_for_doubles(numbers, data);
+}
--- push_swap.c
+++ push_swap.c
@@ -6,94 +6,12 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 11:50:37 by busseven          #+#    #+#             */
-/*   Updated: 2025/02/12 15:01:17 by busseven         ###   ########.fr       */
+/*   Updated: 2025/02/12 15:08:50 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	check_number_count(char **argv)
-{
-	int count;
-	int	i;
-	char **temp;
-
-	count = 0;
-	i = 1;
-	while(argv[i])
-	{
-		temp = ft_split(argv[i], ' ');
-		count += get_2d_length(temp);
-		free_2d_arr(temp);
-		i++;
-	}
-	return(count);
-}
-void	init_data_numbers(char **argv, t_data *data)
-{
-	char	**temp;
-	int		n;
-	int		i;
-	int		k;
-
-	i = 1;
-	n = 0;
-	data->numbers = ft_calloc(check_number_count(argv) + 1, sizeof(char *));
-	while(argv[i])
-	{
-		k = 0;
-		temp = ft_split(argv[i], ' ');
-		while(temp[k])
-		{
-			data->numbers[n] = temp[k];
-			n++;
-			k++;
-		}
-		free(temp);
-		i++;
-	}	
-}
-void	check_arguments(char **numbers, t_data *data)
-{
-	check_for_invalid_char(numbers, data);
-	check_for_non_int(numbers, data);
-	check_for_doubles(numbers, data);
-}
-int		set_index(int content, t_list *a)
-{
-	int i;
-
-	i = 0;
-	while(a)
-	{
-		if(content > a->content)
-			i++;
-		else if(content < a->content)
-			(a->index)++;
-		a = a->next;
-	}
-	return(i);
-}
-
-void	put_nodes_in_a(t_list **a, char **numbers)
-{
-	t_list *new;
-	int	i;
-
-	i = 0;
-	while(numbers[i])
-	{
-		new = ft_lstnew(ft_atoi(numbers[i]));
-		new->index = set_index(new->content, *a);
-		new->target = -1;
-		new->cost = -1;
-		new->position = -1;
-		new->direction = -1;
-		new->is_first = -1;
-		ft_lstadd_back(a, new);
-		i++;
-	}
-}
 void	init_data(t_data *data, char **argv)
 {
 	init_data_numbers(argv, data);
@@ -101,6 +19,7 @@ void	init_data(t_data *data, char **argv)
 	data->a = ft_calloc(1, sizeof(t_list **));
 	data->b = ft_calloc(1, sizeof(t_list **));
 	put_nodes_in_a(data->a, data->numbers);
+	set_real_index(data->a);
 }
 int main(int argc, char **argv)
 {
