5a6ffa4c6d46d969737c6f5d1a2cd6aa08067097
Büşra Seven <152426280+busras3v3n@users.noreply.github.com>
2025-02-14 11:01:25 +0300

    minor changes

--- turk.c
+++ turk.c
@@ -6,7 +6,7 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 17:15:36 by busseven          #+#    #+#             */
-/*   Updated: 2025/02/14 10:02:28 by busseven         ###   ########.fr       */
+/*   Updated: 2025/02/14 10:57:24 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 void	turk(t_data *data)
 {
+	t_list *cheapest;
 	push(data->a, data->b, "b");
 	push(data->a, data->b, "b");
 	set_attributes(data->a, data->b);
